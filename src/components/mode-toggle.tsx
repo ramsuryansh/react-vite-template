d1452f741550a06b2a21e5a84737a0937b0e1b77
Ram Suryawanshi <ram@Rams-MacBook-Pro.local>
2026-04-03 17:13:32 +0530

    initial commit for stater template

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,24 @@
+import { Moon, Sun } from 'lucide-react'
+
+import { useTheme } from '@/components/theme-provider'
+import { Button } from '@/components/ui/button'
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme()
+
+  const toggleTheme = () => {
+    if (theme === 'dark') {
+      setTheme('light')
+    } else {
+      setTheme('dark')
+    }
+  }
+
+  return (
+    <Button variant='outline' size='icon' onClick={toggleTheme}>
+      <Sun className='size-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90' />
+      <Moon className='absolute size-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0' />
+      <span className='sr-only'>Toggle theme</span>
+    </Button>
+  )
+}
